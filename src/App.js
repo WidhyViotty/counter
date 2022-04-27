4c47ac69f78b8f9fb075ecee261c1e4aac8a6453
widhy viotty <widhy@air-de-widhy.lan>
2022-04-27 16:35:19 +0200

    v3

--- src/App.js
+++ src/App.js
@@ -4,30 +4,38 @@ import "./App.css";
 function App() {
   const [counter, setCounter] = useState(0);
 
-  // const increase = () => {
-  //   setCounter((Counter = Counter + 1));
-  // };
-
-  // const decrease = () => {
-  //   setCounter((Counter = Counter - 1));
-  // };
-
-  // const reset = () => {
-  //   setCounter(0);
-  // };
-
   return (
     <div>
-      <span className="output">{counter}</span>
-      <button classeName="button" onClick={setCounter(counter - 1)}>
-        -
-      </button>
-      <button className="button" onClick={setCounter(counter + 1)}>
-        +
-      </button>
-      <button className="button_reset" onClick={setCounter(0)}>
-        Reset
-      </button>
+      <header>
+        <h1>React Counter</h1>
+      </header>
+      <div className="container">
+        <span className="output">{counter}</span>
+        <button
+          classeName="button"
+          onClick={() => {
+            setCounter(counter - 1);
+          }}
+        >
+          -
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            setCounter(counter + 1);
+          }}
+        >
+          +
+        </button>
+        <button
+          className="button_reset"
+          onClick={() => {
+            setCounter(0);
+          }}
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
